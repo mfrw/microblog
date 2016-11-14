58584eb3146b58754c37c0d99df9fd193d28faa0
Muhammad Falak R Wani <falakreyaz@gmail.com>
2016-11-14 20:30:42 +0530

    app/init: update the view for login

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,11 @@ from config import basedir
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = 'login'
+
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 
